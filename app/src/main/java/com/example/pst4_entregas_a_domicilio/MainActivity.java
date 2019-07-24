063f0be302b520649e2f988d3e2895bb47bc9261
jlchuchu <jlchuchu@espol.edu.ec>
2019-07-24 09:29:36 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/pst4_entregas_a_domicilio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pst4_entregas_a_domicilio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
