bfd3d4b68bbcbe365576d20a6f410300df3c5d7e
Jose Chuchuca <jochusua@gmail.com>
2019-07-26 22:14:41 -0500

    Menu prinicipal del clliente y mapa de google

--- app/src/main/java/com/pst/SuperShop/Principal_Cliente.java
+++ app/src/main/java/com/pst/SuperShop/Principal_Cliente.java
@@ -1,35 +1,40 @@
 package com.pst.SuperShop;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Principal_Cliente extends AppCompatActivity {
     private TextView mTextMessage;
-
+    private Fragment selectedFragment=null;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_ubicacion);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_perfil);
-                    return true;
+                    selectedFragment=new home();
+                    break;
+                case R.id.navigation_ubicacion:
+                    Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.navigation_perfil:
+                    selectedFragment=new perfil();
+                    break;
             }
-            return false;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
         }
     };
 
@@ -40,6 +45,7 @@ public class Principal_Cliente extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new home()).commit();
     }
 
 }
