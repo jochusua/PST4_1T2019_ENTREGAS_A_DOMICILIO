8779077e5e272719e1a90bacab98f28efe654128
Cacarden10 <cacarden@espol.edu.ec>
2019-07-27 17:33:08 -0500

    Pantalla de Registro
    Interfaz de registro,  toma de datos

---
+++ app/src/main/java/com/pst/SuperShop/Registro.java
@@ -0,0 +1,25 @@
+package com.pst.SuperShop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Registro extends AppCompatActivity {
+
+    EditText usuario, nombre, apellido, telefono, contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        nombre = (EditText)findViewById(R.id.)
+
+    }
+
+
+
+
+
+}
