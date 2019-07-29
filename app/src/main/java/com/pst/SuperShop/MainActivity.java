7cddecf73547ce83c4073fdd8c6ce3b54f253eda
Cacarden10 <cacarden@espol.edu.ec>
2019-07-29 01:44:00 -0500

    Funcion Login y Registrar
    Se creo la funcion de login y de registro con los parametros de usuario(email) y contraseña mediante el uso del servicio de autentificación que incluye firebase, se validaron los datos y se conecto con la base de autentificacion de usuarios

--- app/src/main/java/com/pst/SuperShop/MainActivity.java
+++ app/src/main/java/com/pst/SuperShop/MainActivity.java
@@ -1,15 +1,29 @@
 package com.pst.SuperShop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText tusuario, tcontraseña;
+    Button btnRegistrar, btnLogin;
+
+    //Declaramos un objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +32,76 @@ public class MainActivity extends AppCompatActivity {
 
         tusuario=(EditText)findViewById(R.id.l_usuario);
         tcontraseña=(EditText)findViewById(R.id.l_contraseña);
+
+        btnLogin = (Button) findViewById(R.id.blogin);
+
+
+        //inicializamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //asociamos un oyente al evento clic del botón
+        btnLogin.setOnClickListener(this);
     }
 
-    public void ingresoCliente(View view) {
-        Intent i = new Intent(this, cliente.class );
-        startActivity(i);
+
+    private void loguearUsuario() {
+        //Obtenemos el email y la contraseña desde las cajas de texto
+        final String email = tusuario.getText().toString().trim();
+        String password = tcontraseña.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if (TextUtils.isEmpty(email)) {//(precio.equals(""))
+            Toast.makeText(this, "Se debe ingresar un email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        //loguear usuario
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()) {
+                            int pos = email.indexOf("@");
+                            String user = email.substring(0, pos);
+                            Toast.makeText(MainActivity.this, "Bienvenido: " + tusuario.getText(), Toast.LENGTH_LONG).show();
+                            //Intent intencion = new Intent(getApplication(), WellcomeActivity.class);
+                            //intencion.putExtra(WellcomeActivity.user, user);
+                            //startActivity(intencion);
+                            Intent i= new Intent(getApplication(), cliente.class );
+                            startActivity(i);
+
+
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {//si se presenta una colisión
+                                Toast.makeText(MainActivity.this, "Ese usuario ya existe ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "No se pudo ingresar con ese usuario ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+
+
     }
+
+
+    @Override
+    public void onClick(View view) {
+        loguearUsuario();
+    }
+
 
     public void FRegistro(View view) {
         Intent i = new Intent(this, Registro.class );
         startActivity(i);
     }
+
 
 }
--- app/src/main/java/com/pst/SuperShop/Registro.java
+++ app/src/main/java/com/pst/SuperShop/Registro.java
@@ -1,29 +1,100 @@
 package com.pst.SuperShop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
 
     EditText etusuario, etnombre, etapellido, ettelefono, etcontraseña;
+    Button btnRegistrar;
+
+    //Declaramos un objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        //inicialización de las variables
         etusuario=(EditText)findViewById(R.id.editusuario);
         etnombre = (EditText)findViewById(R.id.editnombre);
         etapellido=(EditText)findViewById(R.id.editapellido);
         ettelefono = (EditText)findViewById(R.id.edittelefono);
         etcontraseña=(EditText)findViewById(R.id.editcontraseña);
 
+        btnRegistrar =(Button)findViewById(R.id.registrar);
+
+        //inicializamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //attaching listener to button
+        btnRegistrar.setOnClickListener(this);
     }
 
 
 
+    private void Registrar(){
+
+        //Obtenemos el email y la contraseña desde las cajas de texto
+        String email = etusuario.getText().toString().trim();
+        String password  = etcontraseña.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(Registro.this,"Se ha registrado el usuario con el email: "+ etusuario.getText(),Toast.LENGTH_LONG).show();
+                        }else{
+
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {//si se presenta una colisión
+                                Toast.makeText(Registro.this, "Ese usuario ya existe ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(Registro.this, "No se pudo registrar el usuario ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Invocamos al método:
+        Registrar();
+    }
 
 
 }
