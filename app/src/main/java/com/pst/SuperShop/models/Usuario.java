02bd7b2d1b4ca34b532149567c608b36872a9c56
sirDeniel <sirdeniel3@gmail.com>
2019-08-10 18:17:30 -0500

    Clase usuario, ifrroom en cerrarsesion

---
+++ app/src/main/java/com/pst/SuperShop/models/Usuario.java
@@ -0,0 +1,40 @@
+package com.pst.SuperShop.models;
+
+public class Usuario {
+    public String uid;
+    public String email;
+    public String password;
+
+    public Usuario() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
+}
+
