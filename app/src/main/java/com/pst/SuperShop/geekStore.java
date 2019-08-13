b3450a9b4a2febf04327e0616e9371e9ae8a2082
aalcivar <aalcivar04@gmail.com>
2019-08-13 01:18:34 -0500

    Update geekStore.java

--- app/src/main/java/com/pst/SuperShop/geekStore.java
+++ app/src/main/java/com/pst/SuperShop/geekStore.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class geekStore extends AppCompatActivity {
 
     @Override
+    //Nos permite ingresar a la tienda GeekStore
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geek_store);
