91a994d408b3210dd71a4abc189da83ed204346a
Jose Chuchuca <jochusua@gmail.com>
2019-08-17 17:26:59 -0500

    shooping cart

--- app/src/main/java/com/pst/SuperShop/uiModels/shoppingCart.java
+++ app/src/main/java/com/pst/SuperShop/uiModels/shoppingCart.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.pst.SuperShop.R;
 import com.pst.SuperShop.models.DatosItem;
@@ -14,22 +12,23 @@ import com.pst.SuperShop.models.DatosItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StockDeTiendaActivity extends AppCompatActivity {
-
+public class shoppingCart extends AppCompatActivity {
     private List<DatosItem> mDatosItemList;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.stock_de_tienda_activity);
+        setContentView(R.layout.activity_shopping_cart);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mAdapter = new StickerItem(getListData());
-        LinearLayoutManager manager = new LinearLayoutManager(StockDeTiendaActivity.this);
+        LinearLayoutManager manager = new LinearLayoutManager(shoppingCart.this);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(manager);
         mRecyclerView.setAdapter(mAdapter);
     }
+
     private List<DatosItem> getListData() {
         // TODO: Listar items desde la base de datos
         mDatosItemList = new ArrayList<>();
@@ -37,10 +36,5 @@ public class StockDeTiendaActivity extends AppCompatActivity {
             mDatosItemList.add(new DatosItem(i,"Nombre: "+i,"urlnothing"));
         }
         return mDatosItemList;
-    }
-
-    public void ingresarShoppingCart(View view) {
-        Intent i = new Intent(this, shoppingCart.class );
-        startActivity(i);
     }
 }
