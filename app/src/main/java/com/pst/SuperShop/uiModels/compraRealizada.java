c10da4069959dd3bfa08d5133624078fafa83f5d
Jose Chuchuca <jochusua@gmail.com>
2019-08-18 17:29:58 -0500

    activity compra realizada

---
+++ app/src/main/java/com/pst/SuperShop/uiModels/compraRealizada.java
@@ -0,0 +1,43 @@
+package com.pst.SuperShop.uiModels;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.pst.SuperShop.R;
+import com.pst.SuperShop.models.DatosItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class compraRealizada extends AppCompatActivity {
+    private List<DatosItem> mDatosItemList;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compra_realizada);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mAdapter = new StickerItem(getListData());
+        LinearLayoutManager manager = new LinearLayoutManager(compraRealizada.this);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private List<DatosItem> getListData() {
+        // TODO: Listar items desde la base de datos
+        mDatosItemList = (ArrayList<DatosItem>)getIntent().getSerializableExtra("itemsSelecccionados");
+        return mDatosItemList;
+    }
+
+    public void volver(View view) {
+        finish();
+    }
+}
